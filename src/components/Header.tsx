5006b1726ee61736bc82b1d86a5e72b958973ffe
sashko-boiaryn <boyarin.sashko.tbz68@gmail.com>
2022-10-19 00:17:35 +0300

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Box } from "@mui/system";
+
+const Header = () => {
+  return (
+    <>
+      <Container className="header_container">
+        <Box></Box>
+      </Container>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/containers/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import MainPage from "../pages/MainPage";
+import Header from "../components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
