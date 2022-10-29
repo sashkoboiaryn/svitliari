fdcd6f1f457500e294a6d22962d73e853af6b3ca
sashko-boiaryn <boyarin.sashko.tbz68@gmail.com>
2022-10-29 15:17:13 +0300

    29.10-f | prepare project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { Container, Box } from "@mui/system";
+import { Button } from "@mui/material";
+import Stack from "@mui/material/Stack";
 
 const Header = () => {
   return (
     <>
-      <Container className="header_container" sx={{ height: "100hv" }}>
-        <Box>Heade</Box>
+      <Container
+        className="header_container"
+        sx={{
+          height: "100hv",
+          backgroundColor: "#ccc",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box>
+          <Stack spacing={2} direction="row">
+            <Box>LOGO</Box>
+            <Button variant="text">Головна</Button>
+            <Button variant="text">Майстри</Button>
+            <Button variant="text">Про Нас</Button>
+          </Stack>
+        </Box>
       </Container>
     </>
   );
