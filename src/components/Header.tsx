e7f454b546a83cd1c903f895a72fc09da8f1a734
sashko-boiaryn <boyarin.sashko.tbz68@gmail.com>
2022-10-29 17:39:40 +0300

    29.10-f | add styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,23 @@
 import React from "react";
 import { Container, Box } from "@mui/system";
-import { Button } from "@mui/material";
+import { Button, Input, Autocomplete, TextField } from "@mui/material";
 import Stack from "@mui/material/Stack";
 
 const Header = () => {
   return (
     <>
-      <Container
-        className="header_container"
-        sx={{
-          height: "100hv",
-          backgroundColor: "#ccc",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box>
-          <Stack spacing={2} direction="row">
-            <Box>LOGO</Box>
-            <Button variant="text">Головна</Button>
-            <Button variant="text">Майстри</Button>
-            <Button variant="text">Про Нас</Button>
-          </Stack>
+      <div className="header_container">
+        <Box className="header_items_container">
+          <Box className="header_logo">LOGO</Box>
+          {/* <Stack className="header_buttons_box" spacing={2} direction="row"> */}
+          <Button variant="text">Головна</Button>
+          <Button variant="text">Майстри</Button>
+          <Button variant="text">Про Нас</Button>
+          {/* </Stack> */}
+          <input className="header_input" />
+          <Button variant="text">Увійти</Button>
         </Box>
-      </Container>
+      </div>
     </>
   );
 };
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 const MainPage = () => {
   return (
     <div>
+      <Header />
       <h1>MainPage</h1>
     </div>
   );
